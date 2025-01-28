68fe33766016c274034ccb0d1ebd661667192dd8
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-28 18:47:00 +0000

    Formatted with black and flake8

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ VISION_TRACKING_SERVICE_PORT = 8000
 WINDOWS_WEBCAM_SERVICE_PORT = 8001
 
 if __name__ == "__main__":
-    health_check = HealthCheck(VISION_TRACKING_SERVICE_PORT, WINDOWS_WEBCAM_SERVICE_PORT)
+    health_check = HealthCheck(
+        VISION_TRACKING_SERVICE_PORT, WINDOWS_WEBCAM_SERVICE_PORT
+    )
     health_check.check_services()
