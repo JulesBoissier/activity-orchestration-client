fc7024324cf57c560d4fe78932e7d58850101053
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-28 19:20:26 +0000

    Now validates health of wws service using host machine ip

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from src.health_check import HealthCheck
 
+VISION_TRACKING_SERVICE_IP = "127.0.0.1"
 VISION_TRACKING_SERVICE_PORT = 8000
+WINDOWS_WEBCAM_SERVICE_IP = ""  # Host machine's IP
 WINDOWS_WEBCAM_SERVICE_PORT = 8001
 
 if __name__ == "__main__":
     health_check = HealthCheck(
-        VISION_TRACKING_SERVICE_PORT, WINDOWS_WEBCAM_SERVICE_PORT
+        VISION_TRACKING_SERVICE_IP,
+        VISION_TRACKING_SERVICE_PORT,
+        WINDOWS_WEBCAM_SERVICE_IP,
+        WINDOWS_WEBCAM_SERVICE_PORT,
     )
     health_check.check_services()
