68241b9970b153450d7de16b2ae0e220ac91f273
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-01-29 11:45:03 +0000

    Now reading IPs and Ports from dotenv

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+
 from src.health_check import HealthCheck
 
-VISION_TRACKING_SERVICE_IP = "127.0.0.1"
-VISION_TRACKING_SERVICE_PORT = 8000
-WINDOWS_WEBCAM_SERVICE_IP = ""  # Host machine's IP
-WINDOWS_WEBCAM_SERVICE_PORT = 8001
+# Load environment variables from .env file
+load_dotenv()
+
+# Read values from environment variables with fallbacks
+VISION_TRACKING_SERVICE_IP = os.getenv("VISION_TRACKING_SERVICE_IP", "127.0.0.1")
+VISION_TRACKING_SERVICE_PORT = int(os.getenv("VISION_TRACKING_SERVICE_PORT", 8000))
+WINDOWS_WEBCAM_SERVICE_IP = os.getenv("WINDOWS_WEBCAM_SERVICE_IP", "127.0.0.1")
+WINDOWS_WEBCAM_SERVICE_PORT = int(os.getenv("WINDOWS_WEBCAM_SERVICE_PORT", 8001))
+
 
 if __name__ == "__main__":
     health_check = HealthCheck(
