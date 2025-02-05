eade35f8fd730614099d1e0f4a86504847e2bd43
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-02-05 20:50:47 +0000

    Added basic performance monitoring GUI

--- src/user_interfaces/performance_monitoring.py
+++ src/user_interfaces/performance_monitoring.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 from tkinter import messagebox
 from typing import List, Tuple
@@ -9,25 +10,88 @@ class PerformanceMonitoringGUI:
     def __init__(
         self,
         monitor,
-        # positions: List[Tuple[int, int]],
+        nbr_of_points: int,
         wwc: WindowsWebcamClient,
         vtc: VisionTrackingClient,
     ):
-        pass
+        self.started_calibration = False
+        self.wwc = wwc
+        self.vtc = vtc
+        self.positions = [
+            (random.randint(0, monitor.width), random.randint(0, monitor.height))
+            for _ in range(nbr_of_points)
+        ]
+        self.predictions = []
+        self.start_tk(monitor=monitor)
+
+    def start_tk(self, monitor):
+        self.root = tk.Tk()
+
+        self.root.geometry(f"{monitor.width}x{monitor.height}+{monitor.x}+{monitor.y}")
+        self.root.update()
+
+        # Apply fullscreen mode
+        self.root.attributes("-fullscreen", True)
+        self.root.configure(bg="black")
+        self.root.bind("<Escape>", self.exit_app)
+        self.root.bind("<Return>", self.next_position)
+        self.root.bind("<BackSpace>", self.prev_position)
+
+        self.canvas = tk.Canvas(self.root, bg="black", highlightthickness=0)
+        self.canvas.pack(fill=tk.BOTH, expand=True)
+        self.show_start_message()
+        self.root.mainloop()  # Run main event loop inside the class
 
     def show_start_message(self):
-        pass
+        """Displays a text box with instructions."""
+        self.canvas.delete("all")
+        self.canvas.create_text(
+            self.root.winfo_screenwidth() // 2,
+            self.root.winfo_screenheight() // 2,
+            text="Follow the white circle with your gaze and press ENTER. \nPress BACKSPACE to go back. \nPress ENTER to start.",
+            fill="white",
+            font=("Arial", 24, "bold"),
+            justify="center",
+        )
 
     def draw_element(self):
-        pass
+        """Draws the shape at the current position, adjusting for monitor position."""
+        self.canvas.delete("all")  # Clear previous elements
+        x, y = self.positions[self.index]
+        print(x, y)
+
+        # Convert from global screen space to Tkinter's local window space
+        self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="white", outline="")
 
     def next_position(self, event=None):
+        """Move to the next position."""
+        if not self.started_calibration:  # This gets passed the initial message.
+            self.started_calibration = True
+            self.index = 0
+            self.draw_element()
+            return
+
+        image = self.wwc.get_camera_input()  # Capture an image of the face
+        prediction = self.vtc.predict_por(image)
+        self.predictions.append(prediction)
+
+        print(prediction)
+
+        if (
+            self.index < len(self.positions) - 1
+        ):  # While there are positions left, move to next
+            self.index += 1
+            self.draw_element()
+        else:
+            self.report_results()
+            self.exit_app()
+
+    def prev_position(self):
         pass
 
-    def prev_position(self, event=None):
-        """Move back to the previous position."""
-        if self.index > 0:
-            self.images.pop(-1)  # Removes the last image from the list.
+    def report_results(self):
+        pass
 
-            self.index -= 1
-            self.draw_element()
+    def exit_app(self, event=None):
+        """Exit the application."""
+        self.root.destroy()
