e7ef5f6a053a92a212a81453584e0762be2f193a
Jules Boissier <61466984+JulesBoissier@users.noreply.github.com>
2025-02-08 12:29:08 +0000

    Added support for classification metric for screen region prediction

--- src/user_interfaces/performance_monitoring.py
+++ src/user_interfaces/performance_monitoring.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import messagebox
 
 import numpy as np
@@ -16,7 +17,6 @@ class PerformanceMonitoringGUI(BaseGUITest):
         vtc: VisionTrackingClient,
     ):
         super().__init__(monitor, wwc, vtc)
-        import random
 
         self.positions = [
             (random.randint(0, monitor.width), random.randint(0, monitor.height))
@@ -60,6 +60,38 @@ class PerformanceMonitoringGUI(BaseGUITest):
             self.draw_element()
 
     def report_results(self):
+        from src.screen_region import MonitorUtility
+
+        # Create screen regions based on the monitor
+        regions = MonitorUtility.create_screen_region_list(self.monitor, resolution=2)
+
+        # Map positions and predictions to their corresponding regions
+        target_screen_regions = [
+            region
+            for position in self.positions
+            for region in regions
+            if region.is_point_in_region(position[0], position[1])
+        ]
+
+        predict_screen_regions = [
+            region
+            for prediction in self.predictions
+            for region in regions
+            if region.is_point_in_region(prediction[0], prediction[1])
+        ]
+
+        # Compare target and predicted regions
+        results = [
+            (target, prediction, target == prediction)
+            for target, prediction in zip(target_screen_regions, predict_screen_regions)
+        ]
+
+        # Handle the results (e.g., logging or returning for further processing)
+        for target, prediction, is_accurate in results:
+            if is_accurate:
+                print(target, prediction)
+                pass
+
         if len(self.predictions) == len(self.positions):
             errors = np.array(self.predictions) - np.array(self.positions)
             rmse = np.sqrt(np.mean(errors**2))
